fcacaaf1bdd0c95009dda500c16cb30377487978
Aahmed Khan <khanaahmed272@gmail.com>
2024-07-26 16:40:39 +0500

    We learnt how to use input dynamically.

---
+++ Readme.md
@@ -0,0 +1,2 @@
+This is a intial stage of learning python.
+I learnt to use input in this project.
---
+++ app2.py
@@ -0,0 +1,3 @@
+name = input('What is your name? ')
+color = input('Favourite color? ')
+print(name + ' likes ' + color )
